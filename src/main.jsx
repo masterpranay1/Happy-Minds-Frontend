875ee28edaf568eb1175b9d880406a6b8f516eeb
Pranay Raj <58875281+masterpranay1@users.noreply.github.com>
2023-02-24 08:59:14 +0000

    Pranay : Feat ( Added imports in Main )

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css'
 import Home from './pages/Home.page';
+import Header from './components/header'
+import Login from './components/Login'
+import Signup from './components/Signup'
 
 const router = createBrowserRouter([
   {
